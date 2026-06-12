4d8754cd86f1fd54179b2252feef1dc63fb2121d
Evangelos Pafilis <pafilis@hcmr.gr>
2026-06-12 12:42:05 +0300

    Added Version in the Home tab

--- R/mod_home.R
+++ R/mod_home.R
@@ -1,6 +1,6 @@
 
 #' UI Module: Home tab
-#' 
+#'
 #' A Shiny UI module that defines the "Home" tab of the Odyssey app interface.
 #' It displays introductory content, such as app information and
 #' welcome text.
@@ -9,40 +9,40 @@
 #'
 #' @return A Shiny \code{nav_panel} containing the "Home" section layout.
 #'
-#' 
+#'
 #' @export
 #'
 home_ui <- function(id) {
-    
+
     ns <- NS(id)
-    
+
     nav_panel(
         title = tags$h6("Home", style = "color: #004164; margin-bottom: 10px; margin-top: 5px;"),
         fluidPage(br(), uiOutput("home"))
     )
-    
+
 }
 
 
-#'  Server Module: Home Tab 
-#' 
+#'  Server Module: Home Tab
+#'
 #' Defines the server logic for the Home tab of the Odyssey Shiny application.
 #' This tab includes a welcome message, project description, intended audience, methodology,
 #' contribution guidelines, and licensing information.
 #'
 #' @param id Character string specifying the module namespace identifier.
-#' 
+#'
 #' @return This function is called for its side effects to render UI output.
 #'
 #' @export
 #' @importFrom utils URLencode
 home_server <- function(id) {
-    
+
     moduleServer(id, function(input, output, session) {
-        
+
         renderUI(
             HTML("
-            
+
         <div>
           <img src = 'www/logo_nobg.png' width='250' alt='Odyssey Logo' style='float: right; margin-top: -50px; margin-right: -20px;'/>
           <h3 style = 'color: #004164;'>Welcome</h3>
@@ -61,15 +61,18 @@ home_server <- function(id) {
           <h6 style = 'color: #326286;'>Feel free to open a <a href='https://github.com/npechl/MBioG/issues' style='color: #004164;'>GitHub issue</a>
              or contact us via <a href='mailto:inab.bioinformatics@lists.certh.gr' style='color: #004164;'>inab.bioinformatics@lists.certh.gr</a>.</h6>
           <br>
+          <h3 style = 'color: #004164;'>Version</h3>
+          <h6 style = 'color: #326286;'>1.0.1</h6>
+          <br>
           <h3 style = 'color: #004164;'>License</h3>
           <h6 style = 'color: #326286;'>This work, as a whole, is licensed under the <a href='https://github.com/npechl/MBioG/blob/main/LICENSE' style='color: #004164;'>MIT license</a>.</h6>
           <h6 style = 'color: #326286;'>The code contained in this app is simultaneously available under the MIT license;
              this means that you are free to use it in your own packages, as long as you cite the source.</h6>
         </div>
       ")
-            
+
         )
-        
+
     })
-    
+
 }
