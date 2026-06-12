80384140b4c9aa33f17c113febd985f626224ed2
Evangelos Pafilis <pafilis@hcmr.gr>
2026-06-12 15:29:59 +0300

    minor - debugging the multi layer map addition

--- R/app_ui.R
+++ R/app_ui.R
@@ -46,7 +46,7 @@ app_ui <- function(request) {
             map_ui("Map")
 
             # Multilayer - Map tab ----------
-            map_ui("MultiLayerMap")
+            map_multi_layer_ui("MultiLayerMap")
 
         ),
 
